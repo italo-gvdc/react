2322805d428caf0badf3d3632c94c59fa1bed0af
Italo <italogabrielvc@gmail.com>
2022-07-08 13:05:08 -0300

    Criando componentes no react

---
+++ meu-projeto/src/components/HelloWorld.js
@@ -0,0 +1,16 @@
+import Frase from './Frase'
+
+function HellWorld() {
+
+    return (
+        <div>
+            <Frase />
+            <h1>Meu primeiro componente</h1>
+            <Frase />
+            <Frase />
+        </div>
+    )
+
+}
+
+export default HellWorld
