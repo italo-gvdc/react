1abdd10f2bba3641cb001e314a4586b6ec7eaa69
Italo <italogabrielvc@gmail.com>
2022-07-11 10:35:34 -0300

    Promps

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,23 +1,17 @@
 import './App.css';
 import HelloWorld from './components/HelloWorld'
+import SayMyName from './components/SayMyName'
 
 function App() {
-  const name = 'Italo'
-  const newName = name.toUpperCase()
 
-  function sum(a, b) {
-    return a + b
-  }
-
-  const url = 'https://via.placeholder.com/150'
+  const nome  = "Maria"
 
   return (
     <div className="App">
-      <h2>Alterando o JSX</h2>
-      <p>Olá, {newName}</p>
-      <p>Soma: {sum(1, 2)}</p>
-      <img src={url} alt="Minha imagem"/>
       <HelloWorld />
+      <SayMyName nome="Matheus"/>
+      <SayMyName nome="João"/>
+      <SayMyName nome={nome}/>
     </div>
   );
 }
