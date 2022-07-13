f44dbbac3f510e439c32d268b68f35f4f5945ba8
Italo <italogabrielvc@gmail.com>
2022-07-13 10:14:19 -0300

    react fragments

--- meu-projeto/src/components/List.js
+++ meu-projeto/src/components/List.js
@@ -1,5 +1,13 @@
 function List() {
-    return () 
+    return (
+        <>
+            <h1>Minha lista</h1>
+            <ul>
+                <li>Item 1</li>
+                <li>Item 2</li>
+            </ul>
+        </>
+    ) 
 }
 
-export default
+export default List
