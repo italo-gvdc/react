e16a85d6d1cc1b9afea73c554006921b24f8fb47
Italo <italogabrielvc@gmail.com>
2022-07-13 13:27:40 -0300

    Avançando em props

--- meu-projeto/src/components/Item.js
+++ meu-projeto/src/components/Item.js
@@ -1,8 +1,15 @@
-function Item (props) {
+import PropTypes from 'prop-types'
+
+function Item ({ marca, ano_lancamento }) {
     return (
         <>
-            <li>{props.marca}</li>
+            <li>{marca} - {ano_lancamento}</li>
         </>
     )
 }
+
+Item.propTypes = {
+    
+}
+
 export default Item 
