ddeee61779df284b67751f2086830cb6c8c0b20a
Italo <italogabrielvc@gmail.com>
2022-07-14 11:53:24 -0300

    Eventos no react onClick

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,27 +1,13 @@
 import './App.css';
-import SayMyName from './components/SayMyName'
-import Pessoa from './components/Pessoa'
-import Frase from './components/Frase'
-import List from './components/List'
+import Evento from './components/Eventos'
 
 function App() {
-  const nome  = "Maria"
 
   return (
     <div className="App">
-      <h1>Testando CSS</h1>
-      <Frase />
-      <Frase />
-      <SayMyName nome="Matheus"/>
-      <SayMyName nome="João"/>
-      <SayMyName nome={nome}/>
-      <Pessoa 
-      nome="Rodrigo" 
-      idade="28" 
-      profissao="Programador" 
-      foto="https://via.placeholder.com/150"
-      />
-      <List />
+      <h1>Testando Eventos</h1>
+      <Evento />
+
     </div>
   );
 }
