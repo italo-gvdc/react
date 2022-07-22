154c7dc9568d5d8e44b1f2c0b55f285805c3660b
Italo <italogabrielvc@gmail.com>
2022-07-22 13:04:15 -0300

    Criando projeto

--- costs/src/App.js
+++ costs/src/App.js
@@ -1,10 +1,29 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <p>Costs</p>
-    </div>
-  );
+    <Router>
+        <ul>
+          <li>Home</li>
+          <li>Contato</li>
+        </ul>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/company">
+            <Company />
+          </Route>
+          <Route exact path="/contact">
+            <Contact/>
+          </Route>
+          <Route exact path="/newproject">
+            <NewProject />
+          </Route>
+        </Switch>
+        <p>Footer</p>
+    </Router>
+  )
 }
 
-export default App;
+export default App
