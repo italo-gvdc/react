e5b1a13eb7528dc7e38307cac38d985b2689541f
Italo <italogabrielvc@gmail.com>
2022-07-25 12:07:10 -0300

    Roteamento das paginas do projeto

--- costs/src/App.js
+++ costs/src/App.js
@@ -1,27 +1,33 @@
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom'
+import Home from './components/pages/Home'
+import Contact from './components/pages/Contact'
+import Company from './components/pages/Company'
+import NewProject from './components/pages/NewProject'
 
 function App() {
   return (
     <Router>
-        <ul>
-          <li>Home</li>
-          <li>Contato</li>
-        </ul>
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route exact path="/company">
-            <Company />
-          </Route>
-          <Route exact path="/contact">
-            <Contact/>
-          </Route>
-          <Route exact path="/newproject">
-            <NewProject />
-          </Route>
-        </Switch>
-        <p>Footer</p>
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/contact">Contato</Link>
+        <Link to="/company">Empresa</Link>
+        <Link to="/newproject">Novo projeto</Link>
+      </div>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/company">
+          <Company />
+        </Route>
+        <Route exact path="/contact">
+          <Contact />
+        </Route>
+        <Route exact path="/newproject">
+          <NewProject />
+        </Route>
+      </Switch>
+      <p>Footer</p>
     </Router>
   )
 }
