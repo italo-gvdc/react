bf1f4c640716b98411324eb9f8ad2999573219e4
Italo <italogabrielvc@gmail.com>
2022-07-25 12:53:48 -0300

    Estruturando o projeto

--- costs/src/App.js
+++ costs/src/App.js
@@ -4,6 +4,8 @@ import Contact from './components/pages/Contact'
 import Company from './components/pages/Company'
 import NewProject from './components/pages/NewProject'
 
+import Container from './components/layout/Container'
+
 function App() {
   return (
     <Router>
@@ -14,18 +16,20 @@ function App() {
         <Link to="/newproject">Novo projeto</Link>
       </div>
       <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route exact path="/company">
-          <Company />
-        </Route>
-        <Route exact path="/contact">
-          <Contact />
-        </Route>
-        <Route exact path="/newproject">
-          <NewProject />
-        </Route>
+        <Container customClass="min-height">
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/company">
+            <Company />
+          </Route>
+          <Route exact path="/contact">
+            <Contact />
+          </Route>
+          <Route exact path="/newproject">
+            <NewProject />
+          </Route>
+          </Container>
       </Switch>
       <p>Footer</p>
     </Router>
