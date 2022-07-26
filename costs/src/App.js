c7ab17425ad0ad9b7bde372ae93d6cee1c9a6e2d
Italo <italogabrielvc@gmail.com>
2022-07-26 13:42:13 -0300

    Criando Navbar do projeto

--- costs/src/App.js
+++ costs/src/App.js
@@ -5,16 +5,13 @@ import Company from './components/pages/Company'
 import NewProject from './components/pages/NewProject'
 
 import Container from './components/layout/Container'
+import Navbar from './components/layout/Navbar'
+import Footer from './components/layout/Footer'
 
 function App() {
   return (
     <Router>
-      <div>
-        <Link to="/">Home</Link>
-        <Link to="/contact">Contato</Link>
-        <Link to="/company">Empresa</Link>
-        <Link to="/newproject">Novo projeto</Link>
-      </div>
+      <Navbar />
       <Switch>
         <Container customClass="min-height">
           <Route exact path="/">
@@ -31,7 +28,7 @@ function App() {
           </Route>
           </Container>
       </Switch>
-      <p>Footer</p>
+      <Footer />
     </Router>
   )
 }
