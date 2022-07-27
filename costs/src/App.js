bca251814e778eb4d9c480f32b3220600980e8cc
Italo <italogabrielvc@gmail.com>
2022-07-27 13:49:39 -0300

    Criando Navbar e footer

--- costs/src/App.js
+++ costs/src/App.js
@@ -3,6 +3,7 @@ import Home from './components/pages/Home'
 import Contact from './components/pages/Contact'
 import Company from './components/pages/Company'
 import NewProject from './components/pages/NewProject'
+import Projects from './components/pages/Projects'
 
 import Container from './components/layout/Container'
 import Navbar from './components/layout/Navbar'
@@ -16,6 +17,9 @@ function App() {
         <Container customClass="min-height">
           <Route exact path="/">
             <Home />
+          </Route>
+          <Route exact path="/projects">
+            <Projects />
           </Route>
           <Route exact path="/company">
             <Company />
--- costs/src/components/layout/Footer.js
+++ costs/src/components/layout/Footer.js
@@ -1,5 +1,24 @@
+import {FaFacebook, FaInstagram, FaLinkedin} from 'react-icons/fa'
+
+import styles from './Footer.module.css'
+
 function Footer() {
-    return <p>Footer</p>
+    return(<footer>
+        <ul>
+            <li>
+                <FaFacebook />
+            </li>
+            <li>
+                <FaInstagram />
+            </li>
+            <li>
+                <FaLinkedin />
+            </li>
+        </ul>
+        <p><span>Costs</span> &copy; 2021</p>
+    </footer>)
+        
+    
 }
 
 export default Footer
