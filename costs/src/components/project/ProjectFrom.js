30740ab733db45b863bbc60998899f52414e2153
Italo <italogabrielvc@gmail.com>
2022-08-01 12:17:57 -0300

    Componentização de formulario

--- costs/src/components/project/ProjectFrom.js
+++ costs/src/components/project/ProjectFrom.js
@@ -1,14 +1,22 @@
+import Input from '../form/Input'
+
 import styles from './ProjectForm.module.css'
 
 function ProjectForm() {
     return (
         <form className={styles.form}>
-            <div>
-                <input type="text" placeholder="Insira o nome do Projeto" />
-            </div> 
-            <div>
-                <input type="number" placeholder="Insira o orçamento total" />
-            </div>
+            <Input 
+                type="text" 
+                text="Nome do projeto" 
+                name="name" 
+                placeholder="Insira o nome do projeto"
+            />
+            <Input 
+                type="number"
+                text="Orçamento do projeto" 
+                name="budget" 
+                placeholder="Insira o orçamento total"
+            />
             <div>
                 <select name="category_id">
                     <option disabled selected>
