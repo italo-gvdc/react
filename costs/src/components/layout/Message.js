4bf3d279121a2ef0405fb68859ded388539ac184
Italo <italogabrielvc@gmail.com>
2022-08-11 17:24:50 -0300

    Mensagens do sistema

---
+++ costs/src/components/layout/Message.js
@@ -0,0 +1,12 @@
+import { useState, usseEffect } from 'react'
+
+import styles from './Message.module.css'
+
+function Message({ type, msg }) {
+
+    const [visible, setVisible] = useState(false)
+
+    return <div className={`${styles.message} ${styles[type]}`}>{msg}</div>
+}
+
+export default Message
