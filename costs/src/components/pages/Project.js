4f58b11942e7d2ee2e5ec967508d98cd2f2a0555
Italo <italogabrielvc@gmail.com>
2022-08-16 17:08:03 -0300

    Projeto costs

--- costs/src/components/pages/Project.js
+++ costs/src/components/pages/Project.js
@@ -3,14 +3,17 @@ import styles from './Project.module.css'
 import { useParams} from 'react-router-dom'
 import {useState, useEffect} from 'react'
 
+import Loading from '../layout/Loading'
+import Container from '../layout/Container'
+
 function Project() {
     const {id} = useParams()
     
     const [project, setProject] = useState([])
 
     useEffect (() => {
-
-        fetch(`http://localhost:5000/projects/${id}`,{
+        setTimeout(() => {
+            fetch(`http://localhost:5000/projects/${id}`,{
             method: "GET",
             headers: {
                 'Content-Type': 'application/json',
@@ -21,9 +24,27 @@ function Project() {
             setProject(data)
         })
         .catch(err => console.log)
+        }, 300)
     }, [id])
 
-    return <p>{project.name}</p>
+    return (
+        <>
+            {project.name ? (
+                <div>
+                    <Container customClass="column">
+                        <div>
+                            <h1>Projeto: {project.name}</h1>
+                            <button >Editar Projeto</button>
+                        </div>
+                    </Container>
+                </div>
+            ) : (
+              <Loading />
+            )}    
+        </>
+    )
+
+        
 }
 
 export default Project
