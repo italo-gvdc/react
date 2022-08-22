cba1482f90a1b0b4d295b5be26af1253c5397793
Italo <italogabrielvc@gmail.com>
2022-08-22 15:12:44 -0300

    Exibindo os serviços

--- costs/src/components/service/ServiceCard.js
+++ costs/src/components/service/ServiceCard.js
@@ -1,6 +1,12 @@
 import styles from '../project/ProjectCard.module.css'
 
+import {BsFillTrashFill} from 'react-icons/bs'
+
 function ServiceCard({ id, name, cost, description, handleRemove }) {
+
+    const remove = (e) => {
+
+    }
     return (
         <div className={styles.project_card}>
             <h4>{name}</h4>
@@ -8,6 +14,12 @@ function ServiceCard({ id, name, cost, description, handleRemove }) {
                 <span>Custo total:</span> R${cost}
             </p>
             <p>{description}</p>
+            <div className={styles.project_card_actions}>
+                <button onClick={remove}>
+                    <BsFillTrashFill />
+                    Excluir
+                </button>
+            </div>
         </div>
     )
 }
